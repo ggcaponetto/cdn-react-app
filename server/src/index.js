76d6fdc9c006cc05fc13b3dd46331b1a751f9dd3
Giuseppe Caponetto <giuseppe.caponetto@geoimpact.ch>
2020-11-01 17:59:55 +0100

    wip

--- server/src/index.js
+++ server/src/index.js
@@ -1,19 +1,51 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const log = require('loglevel');
 
-const port = process.env.PORT || 3000;
+log.setLevel(log.levels.DEBUG);
 
-app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/index.html`);
-});
+const port = process.env.PORT;
+if (!port) {
+  throw Error('Please define a port to run on.');
+}
 
+class Game {
+  constructor(name = '') {
+    this.name = name;
+    this.sockets = [];
+  }
+
+  addConnections = (sockets) => {
+    this.sockets = [...this.sockets, ...sockets];
+  }
+
+  removeConnection = (socket) => {
+    this.sockets = this.sockets.filter((mySocket) => mySocket.id !== socket.id);
+  };
+
+  toString() {
+    return JSON.stringify({
+      name: this.name,
+      sockets: this.sockets.map((socket) => socket.id),
+    }, null, 4);
+  }
+}
+
+const game = new Game('myGame');
 io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+  game.addConnections([socket]);
+  log.debug('new player connected:\n', game.toString());
+  socket.on('disconnect', () => {
+    game.removeConnection(socket);
+    log.debug('player disconnected:\n', game.toString());
+  });
+
+  socket.on('my-message', (msg) => {
+    io.emit('my-message', msg);
   });
 });
 
 http.listen(port, () => {
-  console.log(`listening on *:${port}`);
+  log.debug(`listening on port ${port}`);
 });
