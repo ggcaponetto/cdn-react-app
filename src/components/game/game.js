0110ff2baf06cc3a5919391260a494d1612b6a31
Giuseppe Caponetto <giuseppe.caponetto@geoimpact.ch>
2020-11-01 13:23:58 +0100

    wip

--- src/components/game/game.js
+++ src/components/game/game.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import * as BABYLON from '@babylonjs/core/Legacy/legacy';
+import * as OIMO from 'oimo/build/oimo';
+import * as CANNON from 'cannon';
+import { jsx, css } from '@emotion/core';
+import metalTexture from './textures/metal-rust.png';
 // this comment tells babel to convert jsx to calls to a function called jsx instead of React.createElement
 /** @jsx jsx */
-import { jsx, css } from '@emotion/core';
 
 const style = css`
   width: 100%;
@@ -19,19 +22,144 @@ export default function Game(props) {
 
     /** ***** Add the create scene function ***** */
     const createScene = function () {
-      // Create the scene space
       const scene = new BABYLON.Scene(engine);
+      scene.clearColor = BABYLON.Color3.Purple();
 
-      // Add a camera to the scene and attach it to the canvas
-      const camera = new BABYLON.ArcRotateCamera('Camera', Math.PI / 2, Math.PI / 2, 2, new BABYLON.Vector3(0, 0, 5), scene);
+      const camera = new BABYLON.FreeCamera('Camera', new BABYLON.Vector3(0, 0, -20), scene);
       camera.attachControl(canvas, true);
+      camera.checkCollisions = true;
+      camera.applyGravity = true;
+      camera.setTarget(new BABYLON.Vector3(0, 0, 0));
 
-      // Add lights to the scene
-      const light1 = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(1, 1, 0), scene);
-      const light2 = new BABYLON.PointLight('light2', new BABYLON.Vector3(0, 1, -1), scene);
+      const light = new BABYLON.DirectionalLight('dir02', new BABYLON.Vector3(0.2, -1, 0), scene);
+      light.position = new BABYLON.Vector3(0, 80, 0);
 
-      // Add and manipulate meshes in the scene
-      const sphere = BABYLON.MeshBuilder.CreateSphere('sphere', { diameter: 2 }, scene);
+      // Material
+      const materialAmiga = new BABYLON.StandardMaterial('amiga', scene);
+      materialAmiga.diffuseTexture = new BABYLON.Texture(metalTexture, scene);
+      materialAmiga.emissiveColor = new BABYLON.Color3(0.5, 0.5, 0.5);
+      materialAmiga.diffuseTexture.uScale = 5;
+      materialAmiga.diffuseTexture.vScale = 5;
+
+      const materialAmiga2 = new BABYLON.StandardMaterial('amiga', scene);
+      materialAmiga2.diffuseTexture = new BABYLON.Texture('textures/amiga.jpg', scene);
+      materialAmiga2.emissiveColor = new BABYLON.Color3(0.5, 0.5, 0.5);
+
+      // Shadows
+      const shadowGenerator = new BABYLON.ShadowGenerator(2048, light);
+
+      // Physics
+      scene.enablePhysics(null, new BABYLON.CannonJSPlugin(true, 10, CANNON));
+      // scene.enablePhysics(null, new BABYLON.OimoJSPlugin('undefined', OIMO));
+
+      // Spheres
+      let y = 0;
+      for (var index = 0; index < 100; index++) {
+        var sphere = BABYLON.Mesh.CreateSphere('Sphere0', 16, 3, scene);
+        sphere.material = materialAmiga;
+
+        sphere.position = new BABYLON.Vector3(Math.random() * 20 - 10, y, Math.random() * 10 - 5);
+
+        shadowGenerator.addShadowCaster(sphere);
+
+        sphere.physicsImpostor = new BABYLON.PhysicsImpostor(sphere, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1 }, scene);
+
+        y += 2;
+      }
+
+      // Link
+      const spheres = [];
+      for (index = 0; index < 10; index++) {
+        sphere = BABYLON.Mesh.CreateSphere('Sphere0', 16, 1, scene);
+        spheres.push(sphere);
+        sphere.material = materialAmiga2;
+        sphere.position = new BABYLON.Vector3(Math.random() * 20 - 10, y, Math.random() * 10 - 5);
+
+        shadowGenerator.addShadowCaster(sphere);
+
+        sphere.physicsImpostor = new BABYLON.PhysicsImpostor(sphere, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1 }, scene);
+      }
+
+      for (index = 0; index < 9; index++) {
+        spheres[index].setPhysicsLinkWith(spheres[index + 1], new BABYLON.Vector3(0, 0.5, 0), new BABYLON.Vector3(0, -0.5, 0));
+      }
+
+      // Box
+      const box0 = BABYLON.Mesh.CreateBox('Box0', 3, scene);
+      box0.position = new BABYLON.Vector3(3, 30, 0);
+      const materialWood = new BABYLON.StandardMaterial('wood', scene);
+      materialWood.diffuseTexture = new BABYLON.Texture('textures/crate.png', scene);
+      materialWood.emissiveColor = new BABYLON.Color3(0.5, 0.5, 0.5);
+      box0.material = materialWood;
+
+      shadowGenerator.addShadowCaster(box0);
+
+      // Compound
+      const part0 = BABYLON.Mesh.CreateBox('part0', 3, scene);
+      part0.position = new BABYLON.Vector3(3, 30, 0);
+      part0.material = materialWood;
+
+      const part1 = BABYLON.Mesh.CreateBox('part1', 3, scene);
+      part1.parent = part0; // We need a hierarchy for compound objects
+      part1.position = new BABYLON.Vector3(0, 3, 0);
+      part1.material = materialWood;
+
+      shadowGenerator.addShadowCaster(part0);
+      shadowGenerator.addShadowCaster(part1);
+      shadowGenerator.useBlurExponentialShadowMap = true;
+      shadowGenerator.useKernelBlur = true;
+      shadowGenerator.blurKernel = 32;
+
+      // Playground
+      const ground = BABYLON.Mesh.CreateBox('Ground', 1, scene);
+      ground.scaling = new BABYLON.Vector3(100, 1, 100);
+      ground.position.y = -5.0;
+      ground.checkCollisions = true;
+
+      const border0 = BABYLON.Mesh.CreateBox('border0', 1, scene);
+      border0.scaling = new BABYLON.Vector3(1, 100, 100);
+      border0.position.y = -5.0;
+      border0.position.x = -50.0;
+      border0.checkCollisions = true;
+
+      const border1 = BABYLON.Mesh.CreateBox('border1', 1, scene);
+      border1.scaling = new BABYLON.Vector3(1, 100, 100);
+      border1.position.y = -5.0;
+      border1.position.x = 50.0;
+      border1.checkCollisions = true;
+
+      const border2 = BABYLON.Mesh.CreateBox('border2', 1, scene);
+      border2.scaling = new BABYLON.Vector3(100, 100, 1);
+      border2.position.y = -5.0;
+      border2.position.z = 50.0;
+      border2.checkCollisions = true;
+
+      const border3 = BABYLON.Mesh.CreateBox('border3', 1, scene);
+      border3.scaling = new BABYLON.Vector3(100, 100, 1);
+      border3.position.y = -5.0;
+      border3.position.z = -50.0;
+      border3.checkCollisions = true;
+
+      const groundMat = new BABYLON.StandardMaterial('groundMat', scene);
+      groundMat.diffuseColor = new BABYLON.Color3(0.5, 0.5, 0.5);
+      groundMat.emissiveColor = new BABYLON.Color3(0.2, 0.2, 0.2);
+      groundMat.backFaceCulling = false;
+      ground.material = groundMat;
+      border0.material = groundMat;
+      border1.material = groundMat;
+      border2.material = groundMat;
+      border3.material = groundMat;
+      ground.receiveShadows = true;
+
+      // Physics
+      box0.physicsImpostor = new BABYLON.PhysicsImpostor(box0, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 2, friction: 0.4, restitution: 0.3 }, scene);
+      ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, friction: 0.5, restitution: 0.7 }, scene);
+      border0.physicsImpostor = new BABYLON.PhysicsImpostor(border0, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0 }, scene);
+      border1.physicsImpostor = new BABYLON.PhysicsImpostor(border1, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0 }, scene);
+      border2.physicsImpostor = new BABYLON.PhysicsImpostor(border2, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0 }, scene);
+      border3.physicsImpostor = new BABYLON.PhysicsImpostor(border3, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0 }, scene);
+
+      part0.physicsImpostor = new BABYLON.PhysicsImpostor(part0, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 2, friction: 0.4, restitution: 0.3 }, scene);
 
       return scene;
     };
