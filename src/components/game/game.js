5dbd0360569d95b73f8c621cb235e3606046537c
Giuseppe Caponetto <giuseppe.caponetto@geoimpact.ch>
2020-11-02 20:10:35 +0100

    wip

--- src/components/game/game.js
+++ src/components/game/game.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import * as BABYLON from '@babylonjs/core/Legacy/legacy';
-import * as OIMO from 'oimo/build/oimo';
+// import * as OIMO from 'oimo/build/oimo';
 import * as CANNON from 'cannon';
 import { jsx, css } from '@emotion/core';
 import * as log from 'loglevel';
@@ -11,14 +11,22 @@ import io from 'socket.io-client';
 import metalTexture from './textures/metal-rust.png';
 
 log.setLevel(log.levels.DEBUG);
+// eslint-disable-next-line max-len
 // this comment tells babel to convert jsx to calls to a function called jsx instead of React.createElement
 /** @jsx jsx */
-
-const style = css`
+const styleDiv = css`
   width: 100%;
   height: 100%;
   margin: 0;
   padding: 0;
+`;
+/** @jsx jsx */
+const performanceMonitorStyle = css`
+  position: fixed;
+  background: black;
+  color: white;
+  left: 0;
+  top: 0;
 `;
 
 class InputController {
@@ -30,17 +38,33 @@ class InputController {
     this.onBeforeRender = () => {
       this.targets.forEach((myTarget) => {
         const myRenderTarget = myTarget;
-        if (this.inputMap.w || this.inputMap.ArrowUp) {
-          myRenderTarget.position.z += this.speed;
+        // Impulse Settings
+        let impulseDirection = null;
+        const impulseMagnitude = 5;
+        const contactLocalRefPoint = BABYLON.Vector3.Zero();
+        const applyImpulse = function () {
+          myRenderTarget.physicsImpostor
+            .applyImpulse(
+              impulseDirection.scale(impulseMagnitude),
+              myRenderTarget.getAbsolutePosition().add(contactLocalRefPoint),
+            );
+        };
+
+        if (this.inputMap.w /* || this.inputMap.ArrowUp */) {
+          impulseDirection = new BABYLON.Vector3(0, 0, 1);
+          applyImpulse();
         }
-        if (this.inputMap.a || this.inputMap.ArrowLeft) {
-          myRenderTarget.position.x -= this.speed;
+        if (this.inputMap.a /* || this.inputMap.ArrowLeft */) {
+          impulseDirection = new BABYLON.Vector3(1, 0, 0);
+          applyImpulse();
         }
-        if (this.inputMap.s || this.inputMap.ArrowDown) {
-          myRenderTarget.position.z -= this.speed;
+        if (this.inputMap.s /* || this.inputMap.ArrowDown */) {
+          impulseDirection = new BABYLON.Vector3(0, 0, -1);
+          applyImpulse();
         }
-        if (this.inputMap.d || this.inputMap.ArrowRight) {
-          myRenderTarget.position.x += this.speed;
+        if (this.inputMap.d /* || this.inputMap.ArrowRight */) {
+          impulseDirection = new BABYLON.Vector3(-1, 0, 0);
+          applyImpulse();
         }
       });
     };
@@ -65,9 +89,13 @@ class InputController {
   }
 }
 
+// eslint-disable-next-line no-unused-vars
 export default function Game(props) {
-  const fnName = 'Game';
+  // const fnName = 'Game';
+  // eslint-disable-next-line no-unused-vars
   const [socket, setSocket] = useState(null);
+  const [fps, setFps] = useState(null);
+  const performanceMonitor = useRef(null);
   useEffect(() => {
     const target = 'http://localhost:3001';
     const mySocket = io(target);
@@ -85,7 +113,6 @@ export default function Game(props) {
   useEffect(() => {
     const canvas = document.getElementById('renderCanvas'); // Get the canvas element
     const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
-
     /** ***** Add the create scene function ***** */
     const createScene = function () {
       const scene = new BABYLON.Scene(engine);
@@ -101,62 +128,23 @@ export default function Game(props) {
       light.position = new BABYLON.Vector3(0, 80, 0);
 
       // Material
-      const materialAmiga = new BABYLON.StandardMaterial('amiga', scene);
-      materialAmiga.diffuseTexture = new BABYLON.Texture(metalTexture, scene);
-      materialAmiga.emissiveColor = new BABYLON.Color3(0.5, 0.5, 0.5);
-      materialAmiga.diffuseTexture.uScale = 5;
-      materialAmiga.diffuseTexture.vScale = 5;
-
-      const materialAmiga2 = new BABYLON.StandardMaterial('amiga', scene);
-      materialAmiga2.diffuseTexture = new BABYLON.Texture('textures/amiga.jpg', scene);
-      materialAmiga2.emissiveColor = new BABYLON.Color3(0.5, 0.5, 0.5);
+      const materialMetal = new BABYLON.StandardMaterial('metalMaterial', scene);
+      materialMetal.diffuseTexture = new BABYLON.Texture(metalTexture, scene);
+      materialMetal.emissiveColor = new BABYLON.Color3(0.5, 0.5, 0.5);
+      materialMetal.diffuseTexture.uScale = 5;
+      materialMetal.diffuseTexture.vScale = 5;
 
       // Shadows
       const shadowGenerator = new BABYLON.ShadowGenerator(2048, light);
 
       // Physics
       scene.enablePhysics(null, new BABYLON.CannonJSPlugin(true, 10, CANNON));
-      // scene.enablePhysics(null, new BABYLON.OimoJSPlugin('undefined', OIMO));
+      const staticElementsOption = {
+        mass: 0,
+        friction: 0.5,
+        restitution: 0.7,
+      };
 
-      // Spheres
-      let y = 0;
-      const controller = new InputController(scene);
-
-      for (let index = 0; index < 5; index++) {
-        const sphere = BABYLON.Mesh.CreateSphere('Sphere0', 16, 3, scene);
-        sphere.material = materialAmiga;
-
-        sphere.position = new BABYLON.Vector3(Math.random() * 20 - 10, y, Math.random() * 10 - 5);
-
-        shadowGenerator.addShadowCaster(sphere);
-
-        sphere.physicsImpostor = new BABYLON.PhysicsImpostor(sphere, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1 }, scene);
-        controller.addTarget(sphere)
-        y += 2;
-      }
-
-      // Box
-      const box0 = BABYLON.Mesh.CreateBox('Box0', 3, scene);
-      box0.position = new BABYLON.Vector3(3, 30, 0);
-      const materialWood = new BABYLON.StandardMaterial('wood', scene);
-      materialWood.diffuseTexture = new BABYLON.Texture('textures/crate.png', scene);
-      materialWood.emissiveColor = new BABYLON.Color3(0.5, 0.5, 0.5);
-      box0.material = materialWood;
-
-      shadowGenerator.addShadowCaster(box0);
-
-      // Compound
-      const part0 = BABYLON.Mesh.CreateBox('part0', 3, scene);
-      part0.position = new BABYLON.Vector3(3, 30, 0);
-      part0.material = materialWood;
-
-      const part1 = BABYLON.Mesh.CreateBox('part1', 3, scene);
-      part1.parent = part0; // We need a hierarchy for compound objects
-      part1.position = new BABYLON.Vector3(0, 3, 0);
-      part1.material = materialWood;
-
-      shadowGenerator.addShadowCaster(part0);
-      shadowGenerator.addShadowCaster(part1);
       shadowGenerator.useBlurExponentialShadowMap = true;
       shadowGenerator.useKernelBlur = true;
       shadowGenerator.blurKernel = 32;
@@ -166,30 +154,60 @@ export default function Game(props) {
       ground.scaling = new BABYLON.Vector3(100, 1, 100);
       ground.position.y = -5.0;
       ground.checkCollisions = true;
+      ground.physicsImpostor = new BABYLON.PhysicsImpostor(
+        ground,
+        BABYLON.PhysicsImpostor.BoxImpostor,
+        staticElementsOption, scene,
+      );
 
       const border0 = BABYLON.Mesh.CreateBox('border0', 1, scene);
       border0.scaling = new BABYLON.Vector3(1, 100, 100);
       border0.position.y = -5.0;
-      border0.position.x = -50.0;
+      border0.position.x = -51.0;
       border0.checkCollisions = true;
+      border0.physicsImpostor = new BABYLON.PhysicsImpostor(
+        border0,
+        BABYLON.PhysicsImpostor.BoxImpostor,
+        staticElementsOption,
+        scene,
+      );
 
       const border1 = BABYLON.Mesh.CreateBox('border1', 1, scene);
       border1.scaling = new BABYLON.Vector3(1, 100, 100);
       border1.position.y = -5.0;
-      border1.position.x = 50.0;
+      border1.position.x = 51.0;
       border1.checkCollisions = true;
+
+      border1.physicsImpostor = new BABYLON.PhysicsImpostor(
+        border1,
+        BABYLON.PhysicsImpostor.BoxImpostor,
+        staticElementsOption,
+        scene,
+      );
 
       const border2 = BABYLON.Mesh.CreateBox('border2', 1, scene);
       border2.scaling = new BABYLON.Vector3(100, 100, 1);
       border2.position.y = -5.0;
-      border2.position.z = 50.0;
+      border2.position.z = 51.0;
       border2.checkCollisions = true;
+      border2.physicsImpostor = new BABYLON.PhysicsImpostor(
+        border2,
+        BABYLON.PhysicsImpostor.BoxImpostor,
+        staticElementsOption,
+        scene,
+      );
 
       const border3 = BABYLON.Mesh.CreateBox('border3', 1, scene);
       border3.scaling = new BABYLON.Vector3(100, 100, 1);
       border3.position.y = -5.0;
-      border3.position.z = -50.0;
+      border3.position.z = -51.0;
       border3.checkCollisions = true;
+      border3.physicsImpostor = new BABYLON.PhysicsImpostor(
+        border3,
+        BABYLON.PhysicsImpostor.BoxImpostor,
+        staticElementsOption,
+        scene,
+      );
 
       const groundMat = new BABYLON.StandardMaterial('groundMat', scene);
       groundMat.diffuseColor = new BABYLON.Color3(0.5, 0.5, 0.5);
@@ -202,15 +220,32 @@ export default function Game(props) {
       border3.material = groundMat;
       ground.receiveShadows = true;
 
-      // Physics
-      box0.physicsImpostor = new BABYLON.PhysicsImpostor(box0, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 2, friction: 0.4, restitution: 0.3 }, scene);
-      ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, friction: 0.5, restitution: 0.7 }, scene);
-      border0.physicsImpostor = new BABYLON.PhysicsImpostor(border0, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0 }, scene);
-      border1.physicsImpostor = new BABYLON.PhysicsImpostor(border1, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0 }, scene);
-      border2.physicsImpostor = new BABYLON.PhysicsImpostor(border2, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0 }, scene);
-      border3.physicsImpostor = new BABYLON.PhysicsImpostor(border3, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0 }, scene);
+      // Spheres
+      let y = 0;
+      const controller = new InputController(scene);
 
-      part0.physicsImpostor = new BABYLON.PhysicsImpostor(part0, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 2, friction: 0.4, restitution: 0.3 }, scene);
+      for (let index = 0; index < 1; index += 1) {
+        const sphere = BABYLON.Mesh.CreateSphere('Sphere0', 16, 3, scene);
+        sphere.physicsImpostor = new BABYLON.PhysicsImpostor(
+          sphere,
+          BABYLON.PhysicsImpostor.SphereImpostor,
+          { mass: 1, friction: 0.1, restitution: 0.3 },
+          scene,
+        );
+        sphere.material = materialMetal;
+        sphere.position = new BABYLON.Vector3(Math.random() * 20 - 10, y, Math.random() * 10 - 5);
+        /*        sphere.physicsImpostor.registerOnPhysicsCollide([
+          border0.physicsImpostor,
+          border1.physicsImpostor,
+          border2.physicsImpostor,
+          border3.physicsImpostor,
+        ], (collider, collideAgainst) => {
+          log.debug('collision', { collider, collideAgainst });
+        }); */
+        shadowGenerator.addShadowCaster(sphere);
+        controller.addTarget(sphere);
+        y += 2;
+      }
 
       return scene;
     };
@@ -218,8 +253,15 @@ export default function Game(props) {
 
     const scene = createScene(); // Call the createScene function
 
+    const myPerformanceMonitor = new BABYLON.PerformanceMonitor(60);
+    myPerformanceMonitor.enable();
+    log.debug('enabled performance monitoring', myPerformanceMonitor);
+    performanceMonitor.current = myPerformanceMonitor;
+
     // Register a render loop to repeatedly render the scene
     engine.runRenderLoop(() => {
+      const myFps = engine.getFps().toFixed();
+      setFps(myFps);
       scene.render();
     });
 
@@ -227,10 +269,22 @@ export default function Game(props) {
     window.addEventListener('resize', () => {
       engine.resize();
     });
-  });
+  }, []);
   return (
-    <div css={style}>
-      <canvas css={style} id="renderCanvas" touch-action="none" />
+    <div css={styleDiv}>
+      <canvas key={1} css={styleDiv} id="renderCanvas" touch-action="none" />
+      <div
+        key={2}
+        css={{
+          position: 'fixed',
+          background: 'black',
+          color: 'white',
+          left: 0,
+          top: 0,
+        }}
+      >
+        {`${fps} fps`}
+      </div>
     </div>
   );
 }
